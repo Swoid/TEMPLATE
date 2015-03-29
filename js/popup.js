8d2122adb11574db04eb70aa94bf4c89159602b6
Adrien Leloup <nittiyh@outlook.com>
2015-03-29 13:59:32 +0200

    Add click out of popup to close

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,24 @@
 	var aPopupButtons = document.querySelectorAll( ".popup-button" );
 	var i = 0, element;
 	for( ; element = aPopupButtons[ i++ ] ; ){
-		element.addEventListener( "click", togglePopup, false  );
+		element.addEventListener( "click", togglePopup, false );
+	}
+
+	document.addEventListener( "click", checkParent, false );
+
+	function checkParent(event){
+		var hasParent = false;
+	    for(var node = event.target; node != document.body; node = node.parentNode)
+	    {
+	      if( node.className == 'messages' || node.className == 'notifications' || node.className == 'search search-primary' || node.className == 'search search-secondary' || node.className == 'footer' ){
+	        hasParent = true;
+	        break;
+	      }
+	    }
+	  if(!hasParent){
+	    var popup = document.querySelector( ".show" );
+		popup.className = "popup";
+	  }
 	}
 
 	function togglePopup(evt){
